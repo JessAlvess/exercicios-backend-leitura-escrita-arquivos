3bbc1c8689af98586dbaabbf9d1b0eeafa7e27ad
Jessé Alves <jessealvespr@outlook.com>
2024-04-15 20:52:34 -0300

    feat: ex2 finalizado

--- 02/src/controladores/pokemon.js
+++ 02/src/controladores/pokemon.js
@@ -3,36 +3,44 @@ const { listarPokemons, detalharPokemon } = require('utils-playground')
 async function listeOsPokemons(req, res) {
     const { paginaDaLista } = req.query
     let listaDePokemons
+   try {
     if (paginaDaLista) {
         listaDePokemons = await listarPokemons(paginaDaLista)
     } else {
         listaDePokemons = await listarPokemons()
     }
 
-    return res.send(listaDePokemons.results)
+    return res.json(listaDePokemons.results)
+   } catch (error) {
+    return res.status(500).json('Erro no servidor')
+   }
 }
 
 async function detalheOPokemon(req, res) {
     const { pokemon } = req.params
     let pokemonDetalhado
-    if (pokemon) {
-        pokemonDetalhado = await detalharPokemon(pokemon)
-    } else {
-        pokemonDetalhado = await detalharPokemon()
+    try {
+        if (pokemon) {
+            pokemonDetalhado = await detalharPokemon(pokemon)
+        } else {
+            pokemonDetalhado = await detalharPokemon()
+        }
+    
+        const objetoPokemon = {
+            "id": pokemonDetalhado.id,
+            "name": pokemonDetalhado.name,
+            "height": pokemonDetalhado.height,
+            "weight": pokemonDetalhado.weight,
+            "base_experience": pokemonDetalhado.base_experience,
+            "forms": pokemonDetalhado.forms,
+            "abilities": pokemonDetalhado.abilities,
+            "species": pokemonDetalhado.species
+        }
+    
+        return res.json(objetoPokemon)
+    } catch (error) {
+    return res.status(500).json('Erro no servidor')
     }
-
-    const objetoPokemon = {
-        "id": pokemonDetalhado.id,
-        "name": pokemonDetalhado.name,
-        "height": pokemonDetalhado.height,
-        "weight": pokemonDetalhado.weight,
-        "base_experience": pokemonDetalhado.base_experience,
-        "forms": pokemonDetalhado.forms,
-        "abilities": pokemonDetalhado.abilities,
-        "species": pokemonDetalhado.species
-    }
-
-    return res.send(objetoPokemon)
 }
 
 module.exports = {
